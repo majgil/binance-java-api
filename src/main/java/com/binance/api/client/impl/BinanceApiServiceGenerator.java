74de24f40f91520b292094bdc6ba8303e4be41e8
mjime127 <mjime127@corp.vodafone.es>
2022-01-22 22:34:39 +0100

    Trazas

--- src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
@@ -1,22 +1,26 @@
 package com.binance.api.client.impl;
 
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+
 import com.binance.api.client.BinanceApiError;
 import com.binance.api.client.config.BinanceApiConfig;
 import com.binance.api.client.exception.BinanceApiException;
 import com.binance.api.client.security.AuthenticationInterceptor;
+
 import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
-import org.apache.commons.lang3.StringUtils;
 import retrofit2.Call;
 import retrofit2.Converter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
-
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Generates a Binance API implementation based on @see {@link BinanceApiService}.
@@ -26,6 +30,9 @@ public class BinanceApiServiceGenerator {
     private static final OkHttpClient sharedClient;
     private static final Converter.Factory converterFactory = JacksonConverterFactory.create();
 
+	public static final String FORMAT_DATE_HOUR_MIN_SEC_ISO = "yyyy-MM-dd'T'HH:mm:ss.S";
+	public static long COUNT_REQUEST = 0;	
+	
     static {
         Dispatcher dispatcher = new Dispatcher();
         dispatcher.setMaxRequestsPerHost(500);
@@ -85,17 +92,40 @@ public class BinanceApiServiceGenerator {
      */
     public static <T> T executeSync(Call<T> call) {
         try {
-            Response<T> response = call.execute();
+        	COUNT_REQUEST++;
+            System.out.println("API BINANCE. Time ini: " + BinanceApiServiceGenerator.formatToString(new Date(), FORMAT_DATE_HOUR_MIN_SEC_ISO) + ". Nº REQUEST : " + COUNT_REQUEST + ". Data: " + call.request());
+            Response<T> response = call.execute();            
             if (response.isSuccessful()) {
+            	System.out.println("API BINANCE. Time fin: " + BinanceApiServiceGenerator.formatToString(new Date(), FORMAT_DATE_HOUR_MIN_SEC_ISO)  + ". Nº REQUEST : " + COUNT_REQUEST + ". Resultado OK. Data: "   + response.raw());
                 return response.body();
             } else {
                 BinanceApiError apiError = getBinanceApiError(response);
+            	System.out.println("API BINANCE. Time fin: " + BinanceApiServiceGenerator.formatToString(new Date(), FORMAT_DATE_HOUR_MIN_SEC_ISO)  + ". Nº REQUEST : " + COUNT_REQUEST + ". Resultado KO. Data: "   + response.raw());
+                
                 throw new BinanceApiException(apiError);
             }
         } catch (IOException e) {
+        	System.out.println("Error al invocar la API. Motivo: " + e.getMessage());
+        	e.printStackTrace();
             throw new BinanceApiException(e);
         }
     }
+    
+	
+	/**
+	 * Transforma una fecha de tipo LocalDate a una String con el formato indicado
+	 * 
+	 * @param fecha Fecha a transformar
+	 * 
+	 * @return Representación en String con el formato indicado de la fecha de tipo LocalDate
+	 */
+	public static String formatToString(Date fecha, String formato) {
+		if (fecha == null) {
+			return null;
+		}
+		final SimpleDateFormat dsf = new SimpleDateFormat(formato);
+		return dsf.format(fecha);
+	}	    
 
     /**
      * Extracts and converts the response error body into an object.
